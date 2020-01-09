651bfd9d0120e94d164e833c7b01c7d332b0f6b7
Irma Castillo <icast073@fiu.edu>
2020-01-09 14:00:17 -0500

    Creating basic structure

---
+++ AspNetCoreOOPRepoPattern.Data/Dto/Interface/IBaseDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreOOPRepoPattern.Data.Dto.Interface
+{
+    public interface IBaseDto<TKey> where TKey : IComparable
+    {
+        TKey Id { get; set; }
+    }
+}
